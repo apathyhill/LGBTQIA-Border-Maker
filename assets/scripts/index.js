8a0632a713a9c268ec9ef7a427e02f4e5c6585f6
apathyhill <apathyhill@gmail.com>
2023-06-01 15:15:35 -0400

    Define constants

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -47,9 +47,15 @@ function flagSetup() {
 // Canvas variables
 let canvas, ctx;
 
+// Drawing constants
+const SIZE_CANVAS = 1280;
+const SIZE_CANVAS_HALF = SIZE_CANVAS * 0.5;
+const SIZE_AVATAR = SIZE_CANVAS * 0.45;
+
 // Avatar
 let imgAvatar = new Image();
 imgAvatar.crossOrigin = "anonymous";
+// Avatar load event
 imgAvatar.addEventListener("load", function(event) {
     // Redraw image
     imageDraw();
@@ -59,10 +65,13 @@ imgAvatar.addEventListener("load", function(event) {
 function imageSetup() {
     console.log("[IMAGE] Getting image canvas...");
 
+    // Get canvas
     canvas = document.getElementById("avatar");
-    canvas.width = 1280;
-    canvas.height = 1280;
+    canvas.width = SIZE_CANVAS;
+    canvas.height = SIZE_CANVAS;
     ctx = canvas.getContext("2d");
+
+    // Set default avatar
     imgAvatar.src = "./assets/images/avatar.png";
 }
 
@@ -71,33 +80,33 @@ function imageDraw() {
     ctx.save(); 
 
     // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, SIZE_CANVAS, SIZE_CANVAS);
 
     // Draw flags
     ctx.imageSmoothingEnabled = false;
-    let flagWidth = canvas.width / flagSelected.length;
+    let flagWidth = SIZE_CANVAS / flagSelected.length;
     flagSelected.forEach(function(flag, i) { 
-        ctx.drawImage(flag, flagWidth * i, 0, flagWidth, canvas.height);
+        ctx.drawImage(flag, flagWidth * i, 0, flagWidth, SIZE_CANVAS);
     });
 
     // Mask out circle
     ctx.beginPath(); 
-    ctx.arc(canvas.width * 0.5, canvas.height * 0.5, canvas.width * 0.45, 0, 2 * Math.PI); 
+    ctx.arc(SIZE_CANVAS_HALF, SIZE_CANVAS_HALF, SIZE_AVATAR, 0, 2 * Math.PI); 
     ctx.clip();
 
     // Draw avatar
     ctx.imageSmoothingEnabled = true;
     var aviW, aviH, aviX, aviY;
 	if (imgAvatar.height > imgAvatar.width) { 
-        aviW = canvas.width;
+        aviW = SIZE_CANVAS;
         aviH = imgAvatar.height * (aviW / imgAvatar.width); 
         aviX = 0; 
-        aviY = (canvas.height - aviH) * 0.5; 
+        aviY = (SIZE_CANVAS - aviH) * 0.5; 
     } else { 
-        aviH = canvas.height;
-        aviW = imgAvatar.width * (aviH/imgAvatar.height);
+        aviH = SIZE_CANVAS;
+        aviW = imgAvatar.width * (aviH / imgAvatar.height);
         aviY = 0;
-        aviX = (canvas.width - aviW) * 0.5;
+        aviX = (SIZE_CANVAS - aviW) * 0.5;
     }
 	ctx.drawImage(imgAvatar, aviX, aviY, aviW, aviH);
 
